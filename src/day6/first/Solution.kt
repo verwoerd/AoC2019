b879a8551bbdd867f145d1a38ed171fc79c4bda7
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-06 07:12:44 +0100

    Day 6 solution

--- src/day6/first/Solution.kt
+++ src/day6/first/Solution.kt
@@ -1,11 +1,29 @@
+package day6.first
+
+
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
-
 fun main() = timeSolution {
+  val pointsMap = readInput()
+  println(pointsMap.keys.map { pointsMap.calculateOrbitsFromOrigin(it) }.sum())
+}
 
+fun readInput(): MutableMap<String, MutableList<String>> =
+  System.`in`.bufferedReader().readLines().map(::parseOrbitLine).fold(mutableMapOf()) { map, (parent, child) ->
+    map.putIfAbsent(parent, mutableListOf())
+    map.putIfAbsent(child, mutableListOf())
+    map[child]!! += parent
+    map
+  }
+
+fun parseOrbitLine(line: String) = line.split(")")
+
+fun Map<String, MutableList<String>>.calculateOrbitsFromOrigin(key: String): Int {
+  var current = key
+  var count = 0
+  while ((this[current] ?: emptyList<String>()).isNotEmpty()) {
+    current = this[current]?.first() ?: error("")
+    count++
+  }
+  return count
 }
--- src/day6/second/Solution.kt
+++ src/day6/second/Solution.kt
@@ -1,11 +1,23 @@
+package day6.second
+
+import day6.first.readInput
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
-
 fun main() = timeSolution {
+  val pointsMap = readInput()
+  val pathOfSanta = pointsMap.calculateOrbitPathToCenterOfMass("SAN")
+  val myPath = pointsMap.calculateOrbitPathToCenterOfMass("YOU")
+  val sharedPath = pathOfSanta.intersect(myPath)
+  println(pathOfSanta.size + myPath.size - 2 * sharedPath.size)
+}
 
+fun Map<String, MutableList<String>>.calculateOrbitPathToCenterOfMass(key: String): List<String> {
+  var current = this[key]?.first() ?: error("")
+  val path = mutableListOf<String>()
+  while ((this[current] ?: emptyList<String>()).isNotEmpty()) {
+    path.add(current)
+    current = this[current]?.first() ?: error("")
+  }
+  path.add(current)
+  return path
 }
