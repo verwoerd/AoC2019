c2d8b7c80a9628bf72e2941d128a96124a1ca010
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-08 08:14:38 +0100

    Day 8 solution

--- src/day8/first/Solution.kt
+++ src/day8/first/Solution.kt
@@ -1,11 +1,19 @@
+package day8.first
+
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
+const val IMAGE_WIDTH = 25
+const val IMAGE_HEIGHT = 6
+const val TOTAL_PIXELS = IMAGE_HEIGHT * IMAGE_WIDTH
 
 fun main() = timeSolution {
-
+  val (ones, twos) = readLine()!!.chunked(TOTAL_PIXELS).minBy { it.count { char -> char == '0' } }!!
+    .fold(Pair(0, 0)) { (ones, twos), char ->
+      when (char) {
+        '1' -> Pair(ones + 1, twos)
+        '2' -> Pair(ones, twos + 1)
+        else -> Pair(ones, twos)
+      }
+    }
+  println(ones * twos)
 }
--- src/day8/second/Solution.kt
+++ src/day8/second/Solution.kt
@@ -1,11 +1,21 @@
+package day8.second
+
+import day8.first.IMAGE_WIDTH
+import day8.first.TOTAL_PIXELS
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
-
 fun main() = timeSolution {
-
+  val result = readLine()!!.chunked(TOTAL_PIXELS).fold("2".repeat(TOTAL_PIXELS).toMutableList()) { acc, layer ->
+    acc.mapIndexed { index, c ->
+      when (c) {
+        '2' -> when (layer[index]) {
+          '1' -> 'X'
+          '2' -> '2'
+          else -> ' '
+        }
+        else -> c
+      }
+    }.toMutableList()
+  }
+  result.chunked(IMAGE_WIDTH).forEach { println(it.joinToString("")) }
 }
