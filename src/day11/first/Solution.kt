d2445e7df017e9a0b398855af3e106e95acdfff9
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-11 07:55:23 +0100

    code clean up for Day 11 solution

--- src/day11/first/Solution.kt
+++ src/day11/first/Solution.kt
@@ -1,5 +1,6 @@
 package day11.first
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.launch
@@ -12,35 +13,42 @@ fun main() = timeSolution {
   val code = readLine()!!.split(",").mapIndexed { index, it -> index.toLong() to it.toLong() }.toMap()
   val outputChannel = Channel<Long>(UNLIMITED)
   val inputChannel = Channel<Long>(UNLIMITED)
-  val area = mutableMapOf<Coordinate, Long>()
-  var currentDirection = Direction.UP
-  var current = Coordinate(0, 0)
-  val path = mutableListOf<Coordinate>()
+  var area: Map<Coordinate, Long> = mutableMapOf()
   runBlocking {
     val program = launch {
       executeBigProgramAsync(code, inputChannel, outputChannel)
     }
     program.start()
-
     inputChannel.send(0L)
-    while (program.isActive) {
-
-      val paint = outputChannel.receive()
-      val direction = outputChannel.receive()
-      area[current] = paint
-      path.add(current)
-      val result = when (direction) {
-        0L -> currentDirection.turnLeft(current)
-        1L -> currentDirection.turnRight(current)
-        else -> throw IllegalArgumentException("Invalid direction recieved")
-      }
-      current = result.first
-      currentDirection = result.second
-      inputChannel.send(area[current]?: 0L)
-    }
+    area =runRobot(program, inputChannel, outputChannel)
   }
   println(area.keys.size)
 }
+
+suspend fun runRobot(
+  program: Job,
+  inputChannel: Channel<Long>,
+  outputChannel: Channel<Long>
+                    ): MutableMap<Coordinate, Long> {
+  val area = mutableMapOf<Coordinate, Long>()
+  var currentDirection = Direction.UP
+  var current = Coordinate(0, 0)
+  while (program.isActive) {
+    val paint = outputChannel.receive()
+    val direction = outputChannel.receive()
+    area[current] = paint
+    val result = when (direction) {
+      0L -> currentDirection.turnLeft(current)
+      1L -> currentDirection.turnRight(current)
+      else -> throw IllegalArgumentException("Invalid direction recieved")
+    }
+    current = result.first
+    currentDirection = result.second
+    inputChannel.send(area[current]?: 0L)
+  }
+  return area
+}
+
 
 enum class Direction {
   UP, DOWN, LEFT, RIGHT;
