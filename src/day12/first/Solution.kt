9949c62a8d171b98fe0afadc3ff34ef207fe330a
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-12 11:23:42 +0100

    Day 12 solution

--- src/day12/first/Solution.kt
+++ src/day12/first/Solution.kt
@@ -1,11 +1,55 @@
-import tools.timeSolution
+package day12.first
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
+import tools.timeSolution
+import java.util.stream.Collectors
+import kotlin.math.abs
+
+const val STEPS = 1000
 
 fun main() = timeSolution {
+  var scan = readPlanetCoordinates()
+  for (step in 1..STEPS) {
+    scan = scan.map { (location, velocity) ->
+      val newVelocity = scan.fold(velocity) { (x, y, z), (nextLocation) ->
+        PlanetCoordinate(
+          x + applyGravity(location.x, nextLocation.x),
+          y + applyGravity(location.y, nextLocation.y),
+          z + applyGravity(location.z, nextLocation.z)
+                        )
+      }
+      applyVelocity(location, newVelocity) to newVelocity
+    }.toMutableList()
+//    scan.printReport(step)
+  }
+  println(scan.map { (location, velocity) -> location.potentialEnergy() * velocity.potentialEnergy() }.sum())
+}
 
+data class PlanetCoordinate(var x: Int, var y: Int, var z: Int) {
+  fun potentialEnergy() = abs(x) + abs(y) + abs(z)
+}
+
+
+val regex = Regex("<x=(-?\\w+), y=(-?\\w+), z=(-?\\w+)>")
+fun readPlanetCoordinates(): MutableList<Pair<PlanetCoordinate, PlanetCoordinate>> =
+  System.`in`.bufferedReader().lines().map { regex.find(it)!!.groups }.map {
+    PlanetCoordinate(
+      it[1]!!.value.toInt(),
+      it[2]!!.value.toInt(),
+      it[3]!!.value.toInt()
+                    ) to PlanetCoordinate(0, 0, 0)
+  }.collect(Collectors.toList())
+
+fun applyGravity(base: Int, other: Int) = when {
+  other > base -> 1
+  other == base -> 0
+  other < base -> -1
+  else -> throw IllegalArgumentException("$base $other")
+}
+
+fun applyVelocity(location: PlanetCoordinate, velocity: PlanetCoordinate) =
+  PlanetCoordinate(location.x + velocity.x, location.y + velocity.y, location.z + velocity.z)
+
+fun MutableList<Pair<PlanetCoordinate, PlanetCoordinate>>.printReport(step: Number) {
+  println("after $step steps")
+  this.forEach { (location, velocity) -> println("pos=<x=${location.x}, y=${location.y}, z=${location.z}>, vel=<x=${velocity.x}, y=${velocity.y}, z=${velocity.z}>") }
 }
--- src/day12/second/Solution.kt
+++ src/day12/second/Solution.kt
@@ -1,11 +1,33 @@
+package day12.second
+
+import day12.first.applyGravity
+import day12.first.readPlanetCoordinates
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
-
 fun main() = timeSolution {
+  val scan = readPlanetCoordinates().map { it.first }
 
+  val xSteps = calculateLoop(scan.map { it.x })
+  val ySteps = calculateLoop(scan.map { it.y })
+  val zSteps = calculateLoop(scan.map { it.z })
+
+  println("Found orbits are repeating for scan x=$xSteps y=$ySteps z=$zSteps")
+
+  println(lcm(xSteps, lcm(ySteps, zSteps)))
+}
+
+fun calculateLoop(startLocation: List<Int>) =
+  generateSequence(Triple(0L, startLocation.toList(), listOf(0, 0, 0, 0))) { (steps, locations, velocity) ->
+    val nextVelocity = velocity.zip(locations.map { location ->
+      locations.map { applyGravity(location, it) }.sum()
+    }, Int::plus)
+    val nextLocations = locations.zip(nextVelocity, Int::plus)
+    Triple(steps + 1, nextLocations, nextVelocity)
+  }.drop(1).first { (_, locations, velocity) -> velocity.all { it == 0 } && locations == startLocation }.first
+
+fun lcm(a: Long, b: Long) = (a * b) / gcd(a, b)
+
+fun gcd(a: Long, b: Long): Long = when (b) {
+  0L -> a
+  else -> gcd(b, a % b)
 }
