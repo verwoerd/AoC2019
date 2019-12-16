03f70784e94c82b1bd6b3a24e57633a15804c911
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-16 12:13:08 +0100

    Day 16 solutions (part 2 not feature complete, but calculates the solution for the test and example input)

--- src/day16/first/Solution.kt
+++ src/day16/first/Solution.kt
@@ -1,11 +1,40 @@
-import tools.timeSolution
+package day16.first
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
+
+import tools.timeSolution
+import kotlin.math.abs
+
 
 fun main() = timeSolution {
+  var input = readLine()!!.toCharArray().map { (it - '0').toLong() }
+  val patterns = patternGenerator(input.size).take(input.size).toList()
+  patterns.map { it.joinToString(",") }.forEach(::println)
+  input = fft(input, patterns)
+  println(input.take(8).joinToString(""))
 
+}
+
+val PATTERN = listOf(0L, 1L, 0L, -1L)
+
+fun patternGenerator(inputSize: Int) = sequence {
+  var currentPattern: LongArray
+  var iteration = 1
+  while (true) {
+    currentPattern = LongArray(inputSize) { PATTERN[((it + 1) / iteration) % (PATTERN.size)] }
+    yield(currentPattern.toList())
+    iteration++
+  }
+}
+
+fun fft(input: List<Long>, patterns: List<List<Long>>): List<Long> {
+  var result: List<Long> = input.toCollection(mutableListOf())
+  var phase = 0
+  while (phase < 100) {
+    result = patterns.map {
+      abs(it.mapIndexed { index, value -> value * result[index] }.sum()) % 10L
+    }
+    phase++
+    println("after phase $phase: ${result.joinToString("")}")
+  }
+  return result
 }
--- src/day16/second/Solution.kt
+++ src/day16/second/Solution.kt
@@ -1,11 +1,50 @@
-import tools.timeSolution
+package day16.second
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
+import tools.timeSolution
+import kotlin.math.abs
+import kotlin.math.min
 
 fun main() = timeSolution {
-
+  val input = readLine()!!.toCharArray().map { (it - '0').toLong() }
+  val offset = input.take(7).joinToString("").toInt()
+  val work = generateInput(input).drop(offset).take(10_000 * input.size - offset).toList()
+  val results = fft(work, offset)
+  println(results.take(8).joinToString(""))
 }
+
+fun generateInput(input: List<Long>) = sequence {
+  var index = -1
+  while (true)
+    yield(input[++index % input.size])
+}
+
+// Observation, numbers are only depend on number following them, so no point calculating the ones before them
+// we can re-use partial sums
+// Note this code only works for offsets in the second half of the list
+fun fft(input: List<Long>, position: Int): List<Long> {
+  var result: List<Long> = input.toCollection(mutableListOf())
+  var phase = 0
+  while (phase < 100) {
+    var start = 0
+    var step = position
+    var sum = 0L
+    var sign = 1L
+    while (start < result.size) {
+      sum += result.subList(start, min(start + step, result.size)).sum() * sign
+      start += 2 * position
+      sign *= -1
+    }
+    result = result.indices.map { index ->
+      if (index != 0) {
+        // Does not deal with the minus part for the first half of the list
+        sum -= result[index - 1]
+        sum += result.subList(min(result.size, step), min(result.size, index + step)).sum()
+      }
+      step++
+      abs(sum) % 10
+    }
+    phase++
+  }
+  return result
+}
+
