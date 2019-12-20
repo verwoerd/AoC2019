35a1d15cb3d798e29a5fccad32ae77c3bd9b20c0
Thomas Verwoerd <thomas@keystone-strategic.com>
2019-12-20 10:19:10 +0100

    Day 20 Solution

--- src/day20/first/Solution.kt
+++ src/day20/first/Solution.kt
@@ -1,11 +1,78 @@
+package day20.first
+
+import tools.Coordinate
+import tools.adjacentCoordinates
+import tools.origin
+import tools.priorityQueueOf
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
+const val START = "AA"
+const val END = "ZZ"
 
 fun main() = timeSolution {
+  val (maze, portals) = parseMaze()
+  println(maze.findPath(portals))
+}
 
+private fun Maze.findPath(portals: Portals): Int {
+  val startCoordinate = portals.getValue(START).first
+  val endCoordinate = portals.getValue(END).first
+  val seen = mutableSetOf(startCoordinate)
+  val portalIndexes = portals.values.filter { it.second != origin }
+    .flatMap { listOf(it.first to it.second, it.second to it.first) }
+    .toMap()
+  val queue = priorityQueueOf(Comparator { o1, o2 -> o1.second.compareTo(o2.second) }, startCoordinate to 0)
+  while (queue.isNotEmpty()) {
+    val (coordinate, distance) = queue.poll()
+    when (coordinate) {
+      endCoordinate -> return distance
+    }
+    adjacentCoordinates(coordinate).filter { seen.add(it) }.filter { (x, y) -> get(y)[x] == '.' }
+      .map { target ->
+        when (target) {
+          in portalIndexes.keys -> portalIndexes.getValue(target) to distance + 2
+          else -> target to distance + 1
+        }
+      }.toCollection(queue)
+  }
+  error("No path found")
+}
+
+typealias Maze = List<CharArray>
+typealias Portals = MutableMap<String, Pair<Coordinate, Coordinate>>
+
+fun parseMaze(): Pair<Maze, Portals> {
+  val input: Maze = System.`in`.bufferedReader().readLines().map { it.toCharArray() }
+  val portals: Portals = mutableMapOf()
+  for (y in 2..input.size - 2) {
+    if (input[y][2] == ' ') continue
+    for (x in 2..input[y].size - 2) {
+      when (input[y][x]) {
+        '.' -> input.checkPortal(x, y, portals)
+      }
+    }
+  }
+  return input to portals
+}
+
+
+private fun Maze.checkPortal(x: Int, y: Int, portals: Portals): Boolean {
+  val label = when {
+    get(y)[x - 1] in 'A'..'Z' -> "${get(y)[x - 2]}${get(y)[x - 1]}"
+    get(y)[x + 1] in 'A'..'Z' -> "${get(y)[x + 1]}${get(y)[x + 2]}"
+    get(y - 1)[x] in 'A'..'Z' -> "${get(y - 2)[x]}${get(y - 1)[x]}"
+    get(y + 1)[x] in 'A'..'Z' -> "${get(y + 1)[x]}${get(y + 2)[x]}"
+    else -> ""
+  }
+  return when (label) {
+    "" -> false
+    in portals.keys -> {
+      portals[label] = portals.getValue(label).first to Coordinate(x, y)
+      true
+    }
+    else -> {
+      portals[label] = Coordinate(x, y) to origin
+      true
+    }
+  }
 }
--- src/day20/second/Solution.kt
+++ src/day20/second/Solution.kt
@@ -1,11 +1,59 @@
+package day20.second
+
+import day20.first.Maze
+import day20.first.Portals
+import day20.first.parseMaze
 import tools.timeSolution
 
-/*
- * Copyright (c) 2019 Keystone Strategic b.v.
- *
- * All rights reserved.
- */
-
 fun main() = timeSolution {
-
+  val (maze, portals) = parseMaze()
+  println(maze.findPath(portals))
 }
+
+private fun Maze.findPath(portals: Portals): Int {
+  val startCoordinate = portals.getValue(day20.first.START).first
+  val endCoordinate = portals.getValue(day20.first.END).first
+  val seen = mutableSetOf(0 to startCoordinate)
+  val portalIndexes = portals.values.filter { it.second != tools.origin }
+    .flatMap { listOf(it.first to it.second, it.second to it.first) }
+    .toMap()
+  val yMax = indexOfLast { it.contains('#') }
+  val xMax = get(yMax).indexOfLast { it == '#' }
+  val outerPortals = portalIndexes.keys.partition { (x, y) -> x == 2 || x == xMax || y == 2 || y == yMax }.first
+
+  // greedily search the paths with lower levels first
+  val queue = tools.priorityQueueOf(
+    Comparator { o1, o2 ->
+      when (val result = o1.third.compareTo(o2.third)) {
+        0 -> o1.second.compareTo(o2.second)
+        else -> result
+      }
+    },
+    Triple(startCoordinate, 0, 0)
+                                   )
+  while (queue.isNotEmpty()) {
+    val (coordinate, distance, level) = queue.poll()
+    when {
+      coordinate == endCoordinate && level == 0 -> return distance
+    }
+    tools.adjacentCoordinates(coordinate).filter { seen.add(level to it) }
+      .filter { (x, y) -> get(y)[x] == '.' }
+      .filter { level != 0 || it !in outerPortals }
+      .map { target ->
+        when (target) {
+          in portalIndexes.keys -> Triple(
+            portalIndexes.getValue(target),
+            distance + 2,
+            when (target) {
+              in outerPortals -> level - 1
+              else -> level + 1
+            }
+                                         )
+          else -> Triple(target, distance + 1, level)
+        }
+      }.toCollection(queue)
+  }
+  error("No path found")
+}
+
+
